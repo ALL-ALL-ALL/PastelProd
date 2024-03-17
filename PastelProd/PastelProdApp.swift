8c6a87f2b70dcc23b945131ce6d21d1b0a1507e5
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-17 02:52:38 +0100

    Initial Commit

---
+++ PastelProd/PastelProdApp.swift
@@ -0,0 +1,17 @@
+//
+//  PastelProdApp.swift
+//  PastelProd
+//
+//  Created by  Ixart on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PastelProdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
